4f02263bb81eaecff7999f762aab4a5b4acd481b
Võ Sơn Hiệp <vosonhiepa@gmail.com>
2018-11-09 16:28:37 +0700

    Mã hóa mật khẩu

--- QuanLyQuanCafe/DAO/AccountDAO.cs
+++ QuanLyQuanCafe/DAO/AccountDAO.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,8 +27,19 @@ namespace QuanLyQuanCafe.DAO
         private AccountDAO() { }
         public bool Login(string userName, string passWord)
         {
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(passWord);
+            byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+
+            string hasPass = "";
+            foreach (byte item in hasData)
+            {
+                hasPass += item;
+            }
+            //var list = hasData.ToString();
+            //list.Reverse();
+
             string query = "USP_Login @userName , @passWord";
-            DataTable result = DataProvider.Instance.ExecuteQuery(query,new object[] { userName, passWord });
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { userName, hasPass });
             return result.Rows.Count > 0;
         }
 
@@ -78,7 +90,7 @@ namespace QuanLyQuanCafe.DAO
 
         public bool ResetPssword(string userName)
         {
-            string query = string.Format("update Account set password = N'0' where UserName = N'{0}'", userName);
+            string query = string.Format("update Account set password = N'1962026656160185351301320480154111117132155' where UserName = N'{0}'", userName);
             int result = DataProvider.Instance.ExecuteNonQuery(query);
             return result > 0;
         }
